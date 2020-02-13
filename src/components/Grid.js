c3444bdb0df93d2683f659229f5f133e1c54f715
jb <dssjon@gmail.com>
2020-02-13 15:56:16 -0800

    Rotations & drop speed handling

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,7 +5,7 @@ import Tetrominos from "./Tetrominos";
 import IntervalHook from "./IntervalHook";
 
 const Grid = () => {
-  const clone = require("rfdc")(); // Returns the deep copy function
+  const clone = require("rfdc")();
   const left_key = 37;
   const right_key = 39;
   const up_key = 38;
@@ -14,16 +14,18 @@ const Grid = () => {
   const enter_key = 13;
   const columns = 10;
   const rows = 20;
-  const default_speed = 1001;
+  const default_speed = 1000;
+  const drop_speed = 5;
   const randomShape = () => {
     const tetrominos = Tetrominos();
     return tetrominos[Math.floor(Math.random() * tetrominos.length)];
   };
-  const initial_shape = randomShape();
   const [grid, setGrid] = useState([]);
-  const [current, setCurrent] = useState(initial_shape);
+  const [current, setCurrent] = useState(randomShape());
   const [frozen, setFrozen] = useState([]);
   const [speed, setSpeed] = useState(default_speed);
+  const [dropInProgress, setDropInProgress] = useState(false);
+  const [preDropSpeed, setPreDropSpeed] = useState(default_speed)
 
   IntervalHook(() => {
     updateGame();
@@ -31,15 +33,17 @@ const Grid = () => {
 
   IntervalHook(() => {
     if (speed > 100) {
-      setSpeed(speed / 1.5);
+      setSpeed(speed / 1.1);
     }
   }, default_speed);
 
   const shiftColumns = dir => {
-    if (isEdge(dir)) return;
+    if (isEdge(dir)) {
+      return;
+    }
     const updated = clone(current);
     updated.shape.map(s => (s.col += dir));
-    return updated;
+    setCurrent(updated);
   };
 
   const getColor = (r, c) => {
@@ -52,33 +56,41 @@ const Grid = () => {
     return "#282c34";
   };
 
-  const checkSquareFrozen = (row, col) => {
-    const isFloor = current.shape.some(r => r.row === 19);
-    const isFrozen = frozen.some(f =>
+  const Collided = (row, col) => {
+    const floor = current.shape.some(r => r.row === rows - 1);
+    const adjacent = frozen.some(f =>
       f.shape.some(s => (s.row === row) & (s.col === col))
     );
-    const isCeiling = isFrozen && current.shape.some(r => r.row === 1);
-    if (isCeiling) {
+    const ceiling = adjacent && current.shape.some(r => r.row === 1);
+    if (ceiling) {
       setSpeed(null);
     }
-    return isFloor || isFrozen;
+    return floor || adjacent;
   };
 
-  const checkFrozen = () => {
-    return current.shape.some(s => checkSquareFrozen(s.row + 1, s.col));
+  const Collision = () => {
+    return current.shape.some(s => Collided(s.row + 1, s.col));
   };
 
   const updateCurrentShape = () => {
-    const isFrozen = checkFrozen();
-    if (isFrozen) setFrozen([...frozen, current]);
-    let updated = isFrozen ? randomShape() : clone(current);
+    const updated = getNextShape();
     updated.shape.map(s => (s.row += 1));
     setCurrent(updated);
   };
 
+  const getNextShape = () => {
+    if (Collision()) {
+      setFrozen([...frozen, current]);
+      resetSpeed();
+      return randomShape();
+    } else {
+      return clone(current);
+    }
+  };
+
   const updateGame = () => {
-    updateGrid();
     updateCurrentShape();
+    updateGrid();
   };
 
   const updateGrid = () => {
@@ -104,17 +116,63 @@ const Grid = () => {
     );
   };
 
+  const rotate = () => {
+    [0, 2, 3].forEach(n => {
+      // Destructure back into current shape excluding pivot vector [1]
+      [current.shape[n].row, current.shape[n].col] = rotateVector(current, n);
+    });
+  };
+
+  const rotateVector = (matrix, idx) => {
+    // Adapted from the math explained here: https://www.youtube.com/watch?v=Atlr5vvdchY
+
+    // Encode vector operation as a rotation of PI/2 (90 degrees)
+    const R1 = [0, -1];
+    const R2 = [1, 0];
+
+    // Get individual square to rotate, i.e. the x/y or row/col coordinates
+    const vector = matrix.shape[idx];
+
+    // Get origin to pivot around which is always the second element (arr[1])
+    const pivot = matrix.shape[1];
+
+    // Get difference between pivot and vector
+    let delta = { row: null, col: null };
+    delta.row = vector.row - pivot.row;
+    delta.col = vector.col - pivot.col;
+
+    // Apply operation R against delta for matrix transformation
+    let transform = { row: null, col: null };
+    transform.row = R1[0] * delta.row + R1[1] * delta.col;
+    transform.col = R2[0] * delta.row + R2[1] * delta.col;
+
+    // Return new position by summing pivot & transform vectors
+    return [pivot.row + transform.row, pivot.col + transform.col];
+  };
+  const resetSpeed = () => {
+    if (dropInProgress) {
+      setDropInProgress(false);
+      setSpeed(preDropSpeed);
+    }
+  };
+
+  const dropShapeFast = () => {
+    setDropInProgress(true);
+    setPreDropSpeed(speed)
+    setSpeed(drop_speed);
+  };
+
   useEffect(() => {
     const handler = event => {
       switch (event.keyCode) {
         case left_key:
-          setCurrent(shiftColumns(-1));
+          shiftColumns(-1);
           break;
         case right_key:
-          setCurrent(shiftColumns(1));
+          shiftColumns(1);
           break;
         case up_key:
-          //rotate();
+          rotate();
           break;
         case esc_key:
           setSpeed(null);
@@ -123,7 +181,7 @@ const Grid = () => {
           //setGameSpeed(currentSpeed);
           break;
         case down_key:
-          //dropShapeFast();
+          dropShapeFast();
           break;
         default:
           break;
